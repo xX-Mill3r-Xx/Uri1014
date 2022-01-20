d1a70ff29d3a4939cf0f85bf4568fad8f422f23a
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-01-20 16:10:18 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Entre com os Km percorridos: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Entre com os litros gastos por Km: ");
+            double y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = x / y;
+            Console.WriteLine(media.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+            Console.ReadLine();
+        }
+    }
+}
